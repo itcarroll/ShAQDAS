96f4c17bff56408b76a414bbf16dd85de05949c6
Ian Carroll <icarroll@sesync.org>
2017-09-01 15:38:41 -0400

    move PGSERVICE definition into .Renviron

--- connect_example.R
+++ connect_example.R
@@ -1,6 +1,10 @@
 library(RPostgreSQL)
 
-Sys.setenv(PGSERVICEFILE='/nfs/bedbugs-data/pg_service.conf')
+# Note: the project contains a .Renviron file, which is used to set the PGSERVICEFILE
+# environment variable.
+
 con <- dbConnect(PostgreSQL(), dbname = 'postgres:///?service=bedbugs')
 
 dbListTables(con)
+
+dbDisconnect(con)
