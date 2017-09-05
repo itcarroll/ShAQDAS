b78d62cef87d283029e623aa04f9662173a47bc7
Ian Carroll <icarroll@sesync.org>
2017-09-05 10:49:49 -0400

    correct service name for connection to registry data

--- connect_example.R
+++ connect_example.R
@@ -3,7 +3,7 @@ library(RPostgreSQL)
 # Note: the project contains a .Renviron file, which is used to set the PGSERVICEFILE
 # environment variable.
 
-con <- dbConnect(PostgreSQL(), dbname = 'postgres:///?service=bedbugs')
+con <- dbConnect(PostgreSQL(), dbname = 'postgres:///?service=registry')
 
 dbListTables(con)
 
